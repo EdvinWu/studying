b4db850d598067f86b3f65f51fb194014847fcc2
Edwin <e.malisevskis@gmail.com>
2016-07-05 10:46:05 +0300

    added Card fields

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,26 @@
 public class Card {
+    private String pin;
+    private int amount;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getPin() {
+
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public Card(String pin, int amount) {
+        this.pin = pin;
+        this.amount = amount;
+    }
 }
