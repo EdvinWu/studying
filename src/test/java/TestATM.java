c00c112f23932f373a0455c884924cf532f3ce66
Nelosh <asakura.jo@gmail.com>
2016-07-05 11:24:32 +0300

    lot of stuff

--- src/test/java/TestATM.java
+++ src/test/java/TestATM.java
@@ -1,6 +1,7 @@
 public class TestATM extends ATM{
-    public TestATM(CardReader cardReader) {
+    public TestATM(CardReader cardReader, Display display) {
         this.cardReader = cardReader;
+        this.display = display;
     }
 
 
